25cd6271995e35ec9cb1ba415dfcc43172cb0cb3
collins <collomash4@gmail.com>
2017-06-13 16:23:57 +0300

    implementing polymorphism

--- myoop.py
+++ myoop.py
@@ -8,8 +8,9 @@ class Doctor:
 	def typhoid(self):
 		print "go for blood test"		
 	def update(self):
-		print "get tested"
-
+		print "please"
+child=Doctor()
+child.cough()
 
 # implementing inheritance the nurse is inheriting from the doctor 
 
@@ -20,4 +21,21 @@ class Nurse(Doctor):
 		pass
 	def getPatient():
 		pass			
-	
+# implementing polymophism We create two classes 
+#  patientOne and patientTwo, 
+#  We then make two instances and call their action using the same method
+class patientOne(object):
+	def prescription(self):
+		print "take 1*3"
+class patientTwo(object):
+	def prescription(self):
+		print "take 3*3"
+
+def getPrescription(pType):
+	pType.prescription()
+
+pOne=patientOne()
+pTwo=patientTwo()
+
+getPrescription(pOne)
+getPrescription(pTwo)
