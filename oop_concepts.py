51e81e8da38863e95f57723d70fd601bb8053e09
collins <collomash4@gmail.com>
2017-06-14 03:02:46 +0300

    I Implemented docstrings and the  double space after initializing a class

--- oop_concepts.py
+++ oop_concepts.py
@@ -1,33 +1,51 @@
+#implementing  encapsulation
 
-#implementing  encapsulation 
 class Doctor:
+
+ 
 	def __init__(self):
 		self.update()
+		
 	def cough(self):
 		print 'take flugone'
+		
 	def typhoid(self):
-		print "go for blood test"		
+		print "go for blood test"
+		
 	def update(self):
 		print "please"
+		
 child=Doctor()
 child.cough()
 
 # implementing inheritance the nurse is inheriting from the doctor 
 
 class Nurse(Doctor):
+
+
 	def __init__(self):
 		self.getPatient()
+		
 	def medicine():
 		pass
+		
 	def getPatient():
-		pass			
-# implementing polymophism We create two classes 
-#  patientOne and patientTwo, 
-#  We then make two instances and call their action using the same method
+		pass
+		
+"""		
+implementing polymophism We create two classes 
+  patientOne and patientTwo, 
+  We then make two instances and call their action using the same method """"  
+  
 class patientOne(object):
+
+
 	def prescription(self):
 		print "take 1*3"
+		
 class patientTwo(object):
+
+
 	def prescription(self):
 		print "take 3*3"
 
